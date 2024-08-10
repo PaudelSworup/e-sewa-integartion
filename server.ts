dff2ab8f3b4c3767a99bdab17903a638e5efe318
paudelSworup <sworupkc9@gmail.com>
2024-08-10 14:35:14 +0545

    asdf ans server

--- server.ts
+++ server.ts
@@ -18,12 +18,20 @@ import {
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
+// e-store-hz3bv8qjo-paudelsworups-projects.vercel.app
+
+const corsOptions = {
+  origin: "https://e-store-omega-beige.vercel.app/", // Your frontend URL
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: "Content-Type,Authorization",
+};
+
 //middlewares
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyparser.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors(corsOptions));
 
 //routes
 app.use("/api", authRoutes);
@@ -32,13 +40,17 @@ app.use("/api", orderRoutes);
 app.use("/api", paymentRoutes);
 
 //for testing
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "public", "test.html"));
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "public", "test.html"));
+// });
+
+app.get("/", async (req, res) => {
+  res.json({ message: "hello there " });
 });
 
-app.get("/email", function (req, res) {
-  res.sendFile(path.join(__dirname, "public", "email.html"));
-});
+// app.get("/email", function (req, res) {
+//   res.sendFile(path.join(__dirname, "public", "email.html"));
+// });
 
 app.listen(port, () => {
   console.log(`Server is Fire at http://localhost:${port}`);
