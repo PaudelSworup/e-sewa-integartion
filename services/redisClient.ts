ad1a51d65cba8030a9e3a43d9ce62ff5018b9ebf
paudelSworup <sworupkc9@gmail.com>
2024-11-09 20:58:50 +0545

    redis configured

--- services/redisClient.ts
+++ services/redisClient.ts
@@ -1,9 +1,7 @@
 // src/config/redisClient.ts
 import { createClient } from "redis";
 
-const redisClient = createClient({
-  url: process.env.REDIS_DEV,
-});
+const redisClient = createClient();
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
 
