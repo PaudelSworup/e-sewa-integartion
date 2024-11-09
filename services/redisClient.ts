19f30f54cfa09a3ffff04206d5a201532326dc70
paudelSworup <sworupkc9@gmail.com>
2024-11-09 20:41:41 +0545

    updated env naming convection

--- services/redisClient.ts
+++ services/redisClient.ts
@@ -2,7 +2,7 @@
 import { createClient } from "redis";
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL,
+  url: process.env.REDIS_DEV,
 });
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
