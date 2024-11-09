2b1a9397b6091d353cb3dc462065c21c99f38004
paudelSworup <sworupkc9@gmail.com>
2024-11-09 21:30:37 +0545

    redis is configured in vercel db

--- services/redisClient.ts
+++ services/redisClient.ts
@@ -1,7 +1,9 @@
 // src/config/redisClient.ts
 import { createClient } from "redis";
 
-const redisClient = createClient();
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
+});
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
 
